bf4e2f62069ea4467e6b9c2a8fae4d42b939d10e
Matheus Alan <matheusalanpv@outlook.com>
2022-02-28 10:32:34 -0300

    Iniciando modelagem entidade ClientModel

--- src/main/java/br/com/starbank/model/AccountModel.java
+++ src/main/java/br/com/starbank/model/AccountModel.java
@@ -1,4 +1,23 @@
 package br.com.starbank.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_accounts")
 public class AccountModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_account")
+    private UUID id;
+//    private ClientModel client;
+    private String accountNumber;
+    private String agencyNumber;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime createdDate;
+
 }
--- src/main/java/br/com/starbank/model/ClientModel.java
+++ src/main/java/br/com/starbank/model/ClientModel.java
@@ -1,4 +1,103 @@
 package br.com.starbank.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_clients")
 public class ClientModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_client")
+    private UUID id;
+    private String cardHolder; //titular do cartão
+    private String mothersName;
+    private String cpf;
+    private String rg;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDateTime birthDate;
+    private String nationality;
+    private String uf;
+//    private List<AccountModel> accountType;
+//    private List<AddressModel> address;
+//    private List<ContactModel> contact;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime entryDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCardHolder() {
+        return cardHolder;
+    }
+
+    public void setCardHolder(String cardHolder) {
+        this.cardHolder = cardHolder;
+    }
+
+    public String getMothersName() {
+        return mothersName;
+    }
+
+    public void setMothersName(String mothersName) {
+        this.mothersName = mothersName;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public LocalDateTime getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDateTime birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public LocalDateTime getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(LocalDateTime entryDate) {
+        this.entryDate = entryDate;
+    }
 }
