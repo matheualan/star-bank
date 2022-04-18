007297844da5055994a50b31c82d661d197b883b
Matheus Alan <matheusalanpv@outlook.com>
2022-04-18 13:27:29 -0300

    Contact Model initialized

--- src/main/java/br/com/starbank/model/ContactModel.java
+++ src/main/java/br/com/starbank/model/ContactModel.java
@@ -1,4 +1,46 @@
 package br.com.starbank.model;
 
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_contact")
 public class ContactModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_contact")
+    private UUID id;
+    private String name;
+    @Column(length = 20)
+    private String cellPhone;
+    private String emailAddress;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(String cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
